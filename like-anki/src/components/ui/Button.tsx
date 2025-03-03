931a233a54da812b50704fa20479cef531e13f8f
layard <114928369+layard32@users.noreply.github.com>
2025-03-03 21:06:15 +0100

    feat: button component

---
+++ like-anki/src/components/ui/Button.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { IconType } from 'react-icons'
+
+interface Props {
+    text?: string;
+    Icon?: IconType;
+    onClickAction: () => void;
+}
+
+const Button: React.FC<Props> = ({text, Icon, onClickAction}: Props) => {
+    return (
+        <div>
+            <button className='btn btn-primary ms-2 my-3'
+                    onClick={onClickAction}> 
+                    { text ? text : null} 
+                    { Icon? <Icon /> : null} </button>
+        </div>
+    )
+};
+
+export default Button;
